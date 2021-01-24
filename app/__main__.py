17c03239116f5478ad0af9ea9e8b397164c13991
Simon Roy <simon.roy1211@gmail.com>
2021-01-24 02:45:03 -0500

    Simplification de asyncio dans main

--- app/__main__.py
+++ app/__main__.py
@@ -1,14 +1,13 @@
 from models import *
 from service import *
 from controller import *
-import time
-import asyncio
+from asyncio import *
 
 async def start():
     await init_start_position()
 
 
 # Démarre la boucle principale
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+loop = new_event_loop()
+set_event_loop(loop)
 loop.run_until_complete(start())
